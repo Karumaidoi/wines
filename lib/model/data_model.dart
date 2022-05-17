1d828ba8bd368a34f470df94bd8232f6d529df6e
Caldewood Alikula <alikulaowen@gmail.com>
2022-05-17 19:23:38 +0300

    Owen changes

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -1,55 +1,25 @@
-class Wines {
-  final String winery;
-  final String wine;
-  final Rating rating;
-  final String location;
-  final String image;
-  final int? id;
+class Coffee {
+  String? title;
+  String? description;
+  List<String>? ingredients;
+  int? id;
 
-  Wines(
-      {required this.winery,
-      required this.wine,
-      required this.rating,
-      required this.location,
-      required this.image,
-      required this.id});
+  Coffee({this.title, this.description, this.ingredients,  this.id});
 
-  static Wines fromJson(json) => Wines(
-        winery: json['winery'],
-        wine: json['wine'],
-        rating: Rating.fromJson(json['rating']),
-        location: json['location'],
-        image: json['image'],
-        id: json['id'],
-      );
+  factory Coffee.fromJson(json) => Coffee(
+    title : json['title'],
+    description :json['description'],
+    ingredients : json['ingredients'].cast<String>(),
+    id : json['id']);
+    
+  
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['winery'] = winery;
-    data['wine'] = wine;
-    data['rating'] = rating.toJson();
-    data['location'] = location;
-    data['image'] = image;
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['title'] = title;
+    data['description'] = description;
+    data['ingredients'] = ingredients;
     data['id'] = id;
-    return data;
-  }
-}
-
-class Rating {
-  String? average;
-  String? reviews;
-
-  Rating({this.average, this.reviews});
-
-  Rating.fromJson(Map<String, dynamic> json) {
-    average = json['average'];
-    reviews = json['reviews'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['average'] = average;
-    data['reviews'] = reviews;
     return data;
   }
 }
